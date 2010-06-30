65eb8065a454f6607007b1b1c9791cfdad82ef29
Ruud Koot <inbox@ruudkoot.nl>
2010-06-30 09:41:20 +0000

    oops

--- src/SystemF/Dimensions.hs
+++ src/SystemF/Dimensions.hs
@@ -32,19 +32,20 @@ dimensions = [ ("L", "m" )
 
 newtype NormalForm = NormalForm (Map.Map Dim Integer) deriving (Show)
 
-base :: Dim -> Set.Set Dim
-base  DimUnit        = Set.empty
-base (DimProd d1 d2) = base d1 `Set.union` base d2
-base (DimInv  d    ) = base d
-base d               = Set.singleton d
 
-exp :: Dim -> Dim -> Integer
-exp  DimUnit        _ = 0
-exp (DimProd d1 d2) v = exp d1 v + exp d2 v
-exp (DimInv  d    ) v = -exp d v
-exp d               v | v == d    = 1
-                      | otherwise = 0
 
 nf :: Dim -> NormalForm
 nf d = NormalForm $ Set.fold (\k -> Map.insert k (exp d k)) Map.empty (base d)
+    where   base :: Dim -> Set.Set Dim
+            base  DimUnit        = Set.empty
+            base (DimProd d1 d2) = base d1 `Set.union` base d2
+            base (DimInv  d    ) = base d
+            base d               = Set.singleton d
+
+            exp :: Dim -> Dim -> Integer
+            exp  DimUnit        _ = 0
+            exp (DimProd d1 d2) v = exp d1 v + exp d2 v
+            exp (DimInv  d    ) v = -exp d v
+            exp d               v | v == d    = 1
+                                  | otherwise = 0
 
